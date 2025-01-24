5ad4ed400aa3c4173f1245bddbe3d94cf5563935
Kairat Karypzhanov <kairat_k@auca.kg>
2025-01-24 23:24:51 +0600

    change files location

--- src/Problems/code_wars/odd_or_even.java
+++
@@ -1,9 +0,0 @@
-public class odd_or_even {
-    public static String oddOrEven (int[] array) {
-      int sum = 0;
-      for (int i = 0; i < array.length; i++) {
-        sum += array[i];
-      }
-      return sum % 2 == 0 ? "even" : "odd";
-    }
-  }
