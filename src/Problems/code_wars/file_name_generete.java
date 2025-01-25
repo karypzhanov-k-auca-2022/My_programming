3a7a7b221247fefe5dabb89eecd132680301244f
Kairat Karypzhanov <kairat_k@auca.kg>
2025-01-25 10:31:40 +0600

    solve random file name generating problem in codewars

---
+++ src/Problems/code_wars/file_name_generete.java
@@ -0,0 +1,15 @@
+package Problems.code_wars;
+
+public class file_name_generete {
+    public static String generateName(PhotoManager photoManager) {
+        String res = "";
+        do {
+            res = "";
+            for (int i = 0; i < 6; i++) {
+                res += (char) (Math.random() * 26 + 'A');
+            }
+        } while (photoManager.nameExists(res));
+
+        return res;
+    }
+}
