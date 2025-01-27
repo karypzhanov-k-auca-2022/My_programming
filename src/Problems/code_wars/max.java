9764bff84e21c3227854275aa50e98c77ebe7022
Kairat Karypzhanov <kairat_k@auca.kg>
2025-01-28 00:15:51 +0600

    solve the max_array_sum problem from codewars

--- src/Problems/code_wars/max.java
+++ src/Problems/code_wars/max.java
@@ -1,13 +1,27 @@
 package Problems.code_wars;
 
 public class max {
-    public static int sequance(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < 0)
-                return 0;
+    public static int sequence(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
         }
 
-        
-        return 0;
+        int maxEndingHere = 0;
+        int maxSoFar = 0;
+        int negativeNums = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                negativeNums++;
+                if (negativeNums == arr.length) {
+                    return 0;
+                }
+            }
+
+            maxEndingHere = Math.max(0, maxEndingHere + arr[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+
+        return maxSoFar;
     }
 }
