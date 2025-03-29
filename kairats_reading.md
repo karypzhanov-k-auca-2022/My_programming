9fa5da79e58d0fe7d4475843deb2c53553cc069c
karypzhanov-k-auca-2022 <kairat_k@auca.kg>
2025-03-29 12:57:40 +0600

    update kairats_reading.md

--- kairats_reading.md
+++ kairats_reading.md
@@ -665,4 +665,6 @@ It tells Spring to create a proxy object and inject it when needed.
 1. `final` - constant value, cannot be changed.
 2. `static` - belongs to the class, not to the object. (for example to count how many objects were created)
     2. We can call static methods without creating an object.
+    2. We can call static methods from another class
+    3. We can't use non-static variables in static methods
    
--- src/practice.java
+++ src/practice.java
@@ -2,13 +2,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class practice {
+    class Human {
+        String name;
+        int age;
+        static int count = 0;
+
+        public Human(String name, int age) {
+            count++;
+            this.name = name;
+            this.age = age;
+        }
+
+        public void showInfo() {
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+        }
+
+        public static void showInfoo() {
+            System.out.println("Total number of humans: " + count);
+        }
+
+        void abc() {
+            age++;
+            count++;
+    }
+
+    static void abcd(){
+        count++;
+    }
+
+
+    
 
     public static void main(String[] args) {
-        List<String> permutations =  new ArrayList<>();
-        // permutations.add("abc");
+        Human human1 = new practice().new Human("Alice", 30);
+        human1.showInfo();
+        Human human2 = new practice().new Human("Bob", 25);
+        human2.showInfo();
 
-        String s = "abc";
-
-        System.out.println(permutations.contains(s));
+        Human.showInfoo();
     }
 }
