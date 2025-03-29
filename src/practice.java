1d28fd2d9da36a5bdd5fa76f33928a773de7f96a
karypzhanov-k-auca-2022 <kairat_k@auca.kg>
2025-03-29 13:17:15 +0600

    remove unused class files and add .gitignore for build artifacts

--- src/practice.java
+++ src/practice.java
@@ -2,36 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class practice {
-    class Human {
-        String name;
-        int age;
-        static int count = 0;
-
-        public Human(String name, int age) {
-            count++;
-            this.name = name;
-            this.age = age;
-        }
-
-        public void showInfo() {
-            System.out.println("Name: " + name);
-            System.out.println("Age: " + age);
-        }
-
-        public static void showInfoo() {
-            System.out.println("Total number of humans: " + count);
-        }
-
-        void abc() {
-            age++;
-            count++;
-        }
-
-        static void abcd() {
-            count++;
-        }
-
-    }
+ 
 
     public static void main(String[] args) {
     }
