d72630f51622d9b2a8e9354c1b7717ab19779510
karypzhanov-k-auca-2022 <kairat_k@auca.kg>
2025-03-30 18:00:36 +0600

    add new practice class and launch configuration; remove obsolete practicee class

---
+++ src/practice/practice.java
@@ -0,0 +1,12 @@
+package practice;
+
+import shapes.Circle;
+
+public class practice {
+    public static void main(String[] args) {
+        Circle circle = new Circle("Circle", "Red", 5.0);
+        circle.printInfo();
+
+        
+    }
+}
