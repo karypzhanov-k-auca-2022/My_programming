fbdd62f90febdabf6c50116abeadaa21cd1467bc
karypzhanov-k-auca-2022 <kairat_k@auca.kg>
2025-04-01 12:10:49 +0600

    remove unused Circle import and related code from practice class

--- src/practice/practice.java
+++ src/practice/practice.java
@@ -1,12 +1,8 @@
 package practice;
 
-import shapes.Circle;
-
 public class practice {
-    public static void main(String[] args) {
-        Circle circle = new Circle("Circle", "Red", 5.0);
-        circle.printInfo();
 
-        
+    public static void main(String[] args) {
+
     }
 }
