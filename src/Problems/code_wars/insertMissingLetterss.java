58bfb4a673349ed49d0cf92ab9096931c083f4b4
karypzhanov-k-auca-2022 <kairat_k@auca.kg>
2025-04-11 18:29:10 +0600

    shoud find the bug

---
+++ src/Problems/code_wars/insertMissingLetterss.java
@@ -0,0 +1,47 @@
+package Problems.code_wars;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class insertMissingLetterss {
+    public static String insertMissingLetters(String str) {
+        StringBuffer res = new StringBuffer();
+
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray(); // lowercase alphabet
+
+        // know the unique characters in the string
+        Set<Character> uniqChars = new HashSet<>();
+        for (char c : str.toCharArray()) {
+            uniqChars.add(c);
+        }
+
+        // know the missing characters in the string
+        Set<Character> missingChars = new HashSet<>();
+        for (char c : alphabet) {
+            if (!uniqChars.contains(c)) {
+                missingChars.add(c);
+            }
+        }
+
+        Set<Character> seen = new HashSet<>();
+        for (char c : str.toCharArray()) {
+
+            res.append(c);
+
+            // if the character is not seen before, add the missing characters
+            if (!seen.contains(c)) {
+                seen.add(c);
+
+                // add new characters to the result a = 97, b = 98, c = 99,
+                for (char missingChar : missingChars) {
+                    if (missingChar > c) {
+                        char bigCharToAdd = Character.toUpperCase(missingChar);
+                        res.append(bigCharToAdd);
+                    }
+                }
+            }
+        }
+
+        return res.toString();
+    }
+}
