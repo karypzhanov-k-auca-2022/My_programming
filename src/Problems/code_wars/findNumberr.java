c884410b6f744bc165698714548ac9f551932b14
karypzhanov-k-auca-2022 <kairat_k@auca.kg>
2025-04-13 15:31:11 +0600

    solve These aren't the numbers you're looking for! (Find a number by approximation) problem form codewars

---
+++ src/Problems/code_wars/findNumberr.java
@@ -0,0 +1,48 @@
+package Problems.code_wars;
+
+import java.util.function.Function;
+
+/*You should find a searched number by approximation.
+
+The searched number will always be between 0 and 100.
+
+You have to write a method, that will get only a function to compare your guess number with the searched number.
+Your method have to find the number with a precision of 5 fractional digits.
+The tolerance for the value: The difference from the searched number must be smaller than 0.00002.
+
+The compare-function, that your method will get as parameter, takes the guessed number as parameter and returns 0 for the correct number, -1 if your number is smaller than the searched number and 1 if your guessed number is greater than the searched number.
+
+Example:
+Searched number: 6
+
+Compare(1); -> -1
+Compare(10); -> 1
+Compare(6); -> 0
+You do not need a Compare-Call, that returns 0. If your number has a precision of 5 fractional digits, and you are sure, it is the searched number, you can return the number.
+
+You will always get the compare-method! So there is no need for a check for null.
+
+Have fun coding it and please don't forget to vote and rank this kata! :-) */
+
+public class findNumberr {
+     public static double findNumber(Function<Double, Integer> compare) {
+        double low = 0.0;
+        double high = 100.0;
+        
+        while (high - low > 0.00001) {
+            double mid = (low + high) / 2.0;
+            
+            int comparisonResult = compare.apply(mid);
+            
+            if (comparisonResult == 0) {
+                return mid;
+            } else if (comparisonResult < 0) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        
+        return (low + high) / 2.0;
+    }
+}
