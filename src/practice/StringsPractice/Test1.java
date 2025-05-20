e221c0072f41c8054801f1f40cb77046bdc7b2b4
karypzhanov-k-auca-2022 <kairat_k@auca.kg>
2025-05-20 13:47:45 +0600

    practice with methods

--- src/practice/StringsPractice/Test1.java
+++ src/practice/StringsPractice/Test1.java
@@ -43,6 +43,55 @@ public class Test1 {
         String email = "user@example.com";
         System.out.println(email.matches("\\w+@\\w+\\.\\w+")); // true
 
-        
+        String word = "ballllldsaddsaloon";
+        System.out.println(word.replaceAll("l", "t")); // "battoondsaddsattoon"
+
+        String text = "banana123";
+        System.out.println(text.replaceAll("\\d", "*")); // "banana*****"
+
+        System.out.println("hello".substring(2)); // "llo"
+
+        // 19. String substring(int beginIndex, int endIndex)
+        System.out.println("hello".substring(1, 4)); // "ell"
+
+        // trim() - удаляет пробелы в начале и конце строки
+        String str = "   Hello, World!   ";
+        String trimmedStr = str.trim();
+        System.out.println("Trimmed string: '" + trimmedStr + "'"); // "Hello, World!"
+
+        // Integer to string
+        int number = 123;
+        String numberString = Integer.toString(number);
+
+        // String to integer
+        String strNumber = "456";
+        int stringToNumber = Integer.parseInt(strNumber);
+        System.out.println("String to integer: " + stringToNumber); // 456
+
+        // String to double
+        String strDouble = "123.45";
+        double stringToDouble = Double.parseDouble(strDouble);
+        System.out.println("String to double: " + stringToDouble); // 123.45
+
+        // double to string
+        double doubleNumber = 789.01;
+        String doubleToString = Double.toString(doubleNumber);
+        System.out.println("Double to string: " + doubleToString); // "789.01"
+        // String to float
+        String strFloat = "123.45";
+        float stringToFloat = Float.parseFloat(strFloat);
+        System.out.println("String to float: " + stringToFloat); // 123.45
+        // float to string
+        float floatNumber = 456.78f;
+        String floatToString = Float.toString(floatNumber);
+        System.out.println("Float to string: " + floatToString); // "456.78"
+        // String to boolean
+        String strBoolean = "true";
+        boolean stringToBoolean = Boolean.parseBoolean(strBoolean);
+        System.out.println("String to boolean: " + stringToBoolean); // true
+        // boolean to string
+        boolean booleanValue = false;
+        String booleanToString = Boolean.toString(booleanValue);
+        System.out.println("Boolean to string: " + booleanToString); // "false"
     }
 }
