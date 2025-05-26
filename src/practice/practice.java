674ae045c12f8841ed73677f116574ec539c1abc
karypzhanov-k-auca-2022 <kairat_k@auca.kg>
2025-05-26 13:20:43 +0600

    remove collarStatus methods and update main method to demonstrate division by zero

--- src/practice/practice.java
+++ src/practice/practice.java
@@ -2,23 +2,9 @@ package practice;
 
 
 public class practice {
-    private boolean collarStatus = false;
-    public boolean isCollarStatus() {
-        return collarStatus;
-    }
-    public void setCollarStatus(boolean status) {
-        collarStatus = status;
-    }
-
-      public static void adopt(practice practice) {
-        System.out.println("Old value of \"collarStatus\" in \"adopt\" method is:" + practice.isCollarStatus());
-        practice.setCollarStatus(true);
-        System.out.println("New value of \"collarStatus\" in \"adopt\" method is:" + practice.isCollarStatus());
-    }
     public static void main(String[] args) {
-        practice kitty = new practice();
-        System.out.println("Old value of \"collarStatus\" in \"main\" method is:" + kitty.isCollarStatus());
-        adopt(kitty);
-        System.out.println("New value of \"collarStatus\" in \"main\" method is:" + kitty.isCollarStatus());
+    int x1 = 10;
+    int x2 = 0;
+    int y = x1/x2;
     }
 }
